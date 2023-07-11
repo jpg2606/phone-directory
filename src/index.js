03590a69514ea729a7952ebc5750d5f2090a7e89
joy2602 <joyprasad112233@gmail.com>
2023-07-11 14:50:54 +0530

    Added ‘PhoneDirectory’ as smart component, renamed ‘App’ as ‘ShowSubscribers’ and converted ‘AddSubscribers’ and ‘ShowSubscribers’ as dumb components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
+import PhoneDirectory from './PhoneDirectory';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <PhoneDirectory />
   </React.StrictMode>
 );
 
